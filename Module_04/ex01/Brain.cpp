92dd9d2f2d52ea3b74fb18c85e3ebf553fa6efb0
Imad Mimouni <imimouni@e2r12p4.1337.ma>
2023-06-20 08:43:25 +0100

    update

---
+++ Module_04/ex01/Brain.cpp
@@ -0,0 +1,32 @@
+
+
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+    std::cout << "Brain default constructor called" << std::endl;
+}
+
+Brain::Brain(Brain &copy)
+{
+    std::cout << "Brain copy constructor called" << std::endl;
+    *this = copy;
+}
+
+Brain::~Brain()
+{
+    std::cout << "Brain default destructor called" << std::endl;
+}
+
+Brain& Brain::operator=(Brain &copy)
+{
+    std::cout << "Brain copy assaignement operator called" << std::endl;
+    if (this != &copy)
+    {
+        for(int i = 0 ; i < 100 ; i++)
+        {
+            this->ideas[i] = copy.ideas[i];
+        }
+    }
+    return (*this);
+}
